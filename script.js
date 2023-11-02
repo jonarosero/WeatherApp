d24b2aa2fe11242402e61744fa210893ae657d41
jonarosero <jars9605@hotmail.com>
2023-11-02 18:06:06 -0500

    modified script

--- script.js
+++ script.js
@@ -1,70 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let weather = {
-    apiKey: "45ad748d8387aeb2bae83fb1a1fa4656",
-    fetchWeather: async function (city) {
-      try {
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
-        );
+let weather = {
+  apiKey: "45ad748d8387aeb2bae83fb1a1fa4656",
+  fetchWeather: async function (city) {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+      );
 
-        if (!response.ok) {
-          alert(`No se encontró el clima para la ciudad de ${city}.`);
-          throw new Error(`No se encontró el clima para la ciudad de ${city}.`);
-        }
-
-        const data = await response.json();
-        this.displayWeather(data);
-      } catch (error) {
-        console.error(error);
+      if (!response.ok) {
+        alert(`No se encontró el clima para la ciudad de ${city}.`);
+        throw new Error(`No se encontró el clima para la ciudad de ${city}.`);
       }
-    },
 
-    displayWeather: function (data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
+      const data = await response.json();
+      this.displayWeather(data);
+    } catch (error) {
+      console.error(error);
+    }
+  },
 
-      console.log(name, icon, description, temp, humidity, speed);
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
 
-      document.querySelector(".city").textContent = `Clima en ${name}`;
-      document.querySelector(
-        ".icon"
-      ).src = `https://openweathermap.org/img/wn/${icon}.png`;
-      document.querySelector(".description").textContent = description;
-      document.querySelector(".temp").textContent = `${temp}°C`;
-      document.querySelector(".humidity").textContent = `Humedad ${humidity}%`;
-      document.querySelector(
-        ".wind"
-      ).textContent = `Velocidad del Viento: ${speed} km/h`;
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    },
+    console.log(name, icon, description, temp, humidity, speed);
 
-    search: function () {
-      const city = document.querySelector(".search-bar").value;
-      if (city) {
-        this.fetchWeather(city);
-      } else {
-        alert("Por favor, ingrese una ciudad.");
-      }
-    },
-  };
+    document.querySelector(".city").textContent = `Clima en ${name}`;
+    document.querySelector(
+      ".icon"
+    ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").textContent = description;
+    document.querySelector(".temp").textContent = `${temp}°C`;
+    document.querySelector(".humidity").textContent = `Humedad ${humidity}%`;
+    document.querySelector(
+      ".wind"
+    ).textContent = `Velocidad del Viento: ${speed} km/h`;
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
 
-  // Agregamos un listener al botón de búsqueda
-  document
-    .querySelector(".search button")
-    .addEventListener("click", function () {
-      weather.search();
-    });
+  search: function () {
+    const city = document.querySelector(".search-bar").value;
+    if (city) {
+      this.fetchWeather(city);
+    } else {
+      alert("Por favor, ingrese una ciudad.");
+    }
+  },
+};
 
-  document
-    .querySelector(".search-bar")
-    .addEventListener("keyup", function (event) {
-      if (event.key == "Enter") {
-        weather.search();
-      }
-    });
-
-  weather.fetchWeather("Denver");
+// Agregamos un listener al botón de búsqueda
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
 });
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Denver");
